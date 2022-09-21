9de998f72b9047ca72b3539444452e8d5b2fe514
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-09-21 14:41:42 +1200

    initial server side set up

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const path = require('path')
 
-const fruitRoutes = require('./routes/fruits')
+const postRoutes = require('./routes/posts')
+const userRoutes = require('./routes/users')
+const commentRoutes = require('./routes/comments')
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.use('/api/v1/fruits', fruitRoutes)
+server.use('/api/v1/posts', postRoutes)
+server.use('/api/v1/users', userRoutes)
+server.use('/api/v1/comments', commentRoutes)
+
+server.get('*', (req, res) => {
+  res.sendFile(path.resolve('server/public/index.html'))
+})
 
 module.exports = server
