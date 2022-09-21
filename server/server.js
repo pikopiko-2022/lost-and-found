aa184e4b3a51163f1dcb99a047b6db76c1e3d27c
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-09-21 15:36:22 +1200

    remove user route stuff

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 
 const postRoutes = require('./routes/posts')
-const userRoutes = require('./routes/users')
 const commentRoutes = require('./routes/comments')
 
 const server = express()
@@ -11,11 +10,6 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/posts', postRoutes)
-server.use('/api/v1/users', userRoutes)
 server.use('/api/v1/comments', commentRoutes)
-
-server.get('*', (req, res) => {
-  res.sendFile(path.resolve('server/public/index.html'))
-})
 
 module.exports = server
