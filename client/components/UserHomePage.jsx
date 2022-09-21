04a70662df547b45e28119aa8a9fccf32e507131
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-09-21 22:21:35 +1200

    can pull data through from posts

--- client/components/UserHomePage.jsx
+++ client/components/UserHomePage.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getAllPosts } from '../apis/posts'
+
 export default function UserHomePage() {
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    getAllPosts()
+      .then((result) => {
+        setPosts(result)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }, [])
+
   return (
     <>
       <p>UserHomePage</p>
+      {posts.map((post) => {
+        return <div key={post.id}>{post.title}</div>
+      })}
     </>
   )
 }
