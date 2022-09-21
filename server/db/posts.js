17e5660ff9666a677ad6f645b0e356c72c81aeda
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-09-21 16:12:26 +1200

    can get all posts from database

--- server/db/posts.js
+++ server/db/posts.js
@@ -1,1 +1,17 @@
-//db functions for posts to go here
+const config = require('./knexfile').development
+const connection = require('knex')(config)
+
+function getAllPosts(db = connection) {
+  return db('posts').select(
+    'id',
+    'uploader_id as uploaderId',
+    'category',
+    'title',
+    'date',
+    'description',
+    'image_url as imageUrl',
+    'location'
+  )
+}
+
+module.exports = { getAllPosts }
