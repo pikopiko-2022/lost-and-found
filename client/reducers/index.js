f5cf6e482f381ebbf4ba34222765fb8e2bdc6566
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-09-23 10:18:59 +1200

    remove comments from reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,10 +2,8 @@ import { combineReducers } from 'redux'
 
 import postsReducer from './posts'
 import usersReducer from './users'
-import comments from './comments'
 
 export default combineReducers({
   usersReducer,
   postsReducer,
-  comments,
 })
