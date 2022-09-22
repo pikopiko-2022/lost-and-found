c7898f3df6184fb07da8bcdf056ad7d7c74613c4
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-09-22 12:40:58 +1200

    make join between users and posts

--- server/db/posts.js
+++ server/db/posts.js
@@ -2,16 +2,20 @@ const config = require('./knexfile').development
 const connection = require('knex')(config)
 
 function getAllPosts(db = connection) {
-  return db('posts').select(
-    'id',
-    'uploader_id as uploaderId',
-    'category',
-    'title',
-    'date',
-    'description',
-    'image_url as imageUrl',
-    'location'
-  )
+  return db('posts')
+    .join('users', 'posts.uploader_id', 'users.auth0_id')
+    .select(
+      'users.name as uploaderName',
+      'users.location as userLocation',
+      'posts.id as id',
+      'posts.uploader_id as uploaderId',
+      'posts.category as category',
+      'posts.title as title',
+      'posts.date as date',
+      'posts.description as description',
+      'posts.image_url as imageUrl',
+      'posts.location as itemLocation'
+    )
 }
 
 function addPost(post, db = connection) {
