60280f80234bd95a4acc7284887aa498a4de7a66
Sam Dransfield <107464658+sam-nikau@users.noreply.github.com>
2022-09-22 14:52:04 +1200

    db/routes tests complete

--- server/routes/commentsRoutes.test.js
+++ server/routes/commentsRoutes.test.js
@@ -1,9 +1,6 @@
 const request = require('supertest')
-
 const server = require('../server')
-
 const db = require('../db/comments')
-
 const { getComments } = require('../db/comments')
 
 jest.mock('../db/comments')
@@ -17,28 +14,21 @@ afterEach(() => {
 describe('GET /comments', () => {
   test('renders comments', () => {
     db.getComments.mockReturnValue(Promise.resolve([{ id: '1' }, { id: '2' }]))
-
     return request(server)
       .get('/api/v1/comments/')
-
       .then((res) => {
         expect(res.body).toHaveLength(2)
-
         expect(res.body[0].id).toBe('1')
       })
   })
 
   test('returns status 500 and consoles error', () => {
     getComments.mockImplementation(() => Promise.reject(new Error('we woow')))
-
     console.error.mockImplementation(() => {})
-
     return request(server)
       .get('/api/v1/comments/')
-
       .then((res) => {
         expect(res.status).toBe(500)
-
         expect(console.error).toHaveBeenCalledWith('we woow')
       })
   })
