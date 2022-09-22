ae46f68765ecd4748fd56645391554fbf09b342e
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-09-22 15:10:26 +1200

    basic form complete

--- client/components/AllPosts.jsx
+++ client/components/AllPosts.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchPosts } from '../actions/posts'
 import Post from './Post'
@@ -13,6 +14,7 @@ export default function AllPosts() {
 
   return (
     <>
+      <Link to="/createPost">Create a post</Link>
       {posts.map((post) => {
         return (
           <Post
