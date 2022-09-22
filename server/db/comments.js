4c8f12e0be9c791ad37a50c9d1082e51f277ea6e
Sam Dransfield <107464658+sam-nikau@users.noreply.github.com>
2022-09-22 12:24:59 +1200

    initial commit - basic frontend/backend setup

--- server/db/comments.js
+++ server/db/comments.js
@@ -1,1 +1,20 @@
 //db functions for comments to go here
+const config = require('./knexfile').development
+const connection = require('knex')(config)
+
+function getComments(db = connection) {
+  return db('comments').select(
+    'id',
+    'commenter_id as commenterId',
+    'date_commented as dateCommented',
+    'comment'
+  )
+}
+
+function addComment(comment, db = connection) {
+  return db('comments')
+    .insert(comment)
+    .then(() => getComments(db))
+}
+
+module.exports = { getComments, addComment }
