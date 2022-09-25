001867fb469b91dbc7005256d314f91b6efa8f5a
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-09-25 15:26:47 +1300

    CreateComment component test

--- client/components/__tests__/CreateComment.test.jsx
+++ client/components/__tests__/CreateComment.test.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { fireEvent, render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { useDispatch } from 'react-redux'
 import CreateComment from '../CreateComment'
@@ -9,21 +8,23 @@ import { fetchPosts } from '../../actions/posts'
 
 jest.mock('../../actions/posts')
 jest.mock('react-redux')
-
-const fakeFormData = {
-  post_id: 1,
-  date_commented: new Date().toDateString(),
-  comment: 'I found your scarf',
-}
+jest.mock('../../apis/comments.api')
 
 const fakeDispatch = jest.fn()
 useDispatch.mockReturnValue(fakeDispatch)
-// addComment().mockReturnValue(Promise.resolve('Comment added'))
+addComment.mockReturnValue('Comment added')
+fetchPosts.mockReturnValue('Posts fetched')
 
 describe('<CreateComment />', () => {
   it('renders a form', () => {
     render(<CreateComment postId={1} />)
     const form = screen.getByRole('form')
     expect(form).toBeInTheDocument()
+  })
+  test('clicking the submit button makes fetch posts run', () => {
+    render(<CreateComment postId={1} />)
+    const button = screen.getByRole('button')
+    fireEvent.click(button)
+    expect(fakeDispatch).toHaveBeenCalledWith('Posts not fetched')
   })
 })
