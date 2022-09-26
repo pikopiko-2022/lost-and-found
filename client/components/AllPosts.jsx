92cf949a4d8de6218182d4367cc827ec21b333d9
Daniel Pryor <75132443+daniel-pryor@users.noreply.github.com>
2022-09-26 14:00:29 +1300

    remove reverse from client side to server side

--- client/components/AllPosts.jsx
+++ client/components/AllPosts.jsx
@@ -16,27 +16,23 @@ export default function AllPosts() {
       <Link to="/createPost">
         <button>Create a post</button>
       </Link>
-      {posts
-        .slice(0)
-        .reverse()
-        .map((post) => {
-          return (
-            <Post
-              key={post.id}
-              id={post.id}
-              uploaderId={post.uploaderId}
-              title={post.title}
-              uploader={post.username}
-              category={post.category}
-              date={post.date}
-              description={post.description}
-              image={post.imageUrl}
-              location={post.itemLocation}
-              comments={post.comments}
-            />
-          )
-        })}
-
+      {posts.map((post) => {
+        return (
+          <Post
+            key={post.id}
+            id={post.id}
+            uploaderId={post.uploaderId}
+            title={post.title}
+            uploader={post.username}
+            category={post.category}
+            date={post.date}
+            description={post.description}
+            image={post.imageUrl}
+            location={post.itemLocation}
+            comments={post.comments}
+          />
+        )
+      })}
     </>
   )
 }
