299ce3b84e3dcbd44129d875deea6a783b70ae81
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-09-27 10:02:41 +1300

    amend Comment component test

--- client/components/__tests__/Comment.test.jsx
+++ client/components/__tests__/Comment.test.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
 import { screen, render } from '@testing-library/react'
+import '@testing-library/jest-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import Comment from '../Comment'
+import userEvent from '@testing-library/user-event'
+import { deleteComment } from '../../apis/comments'
+import { fetchPosts } from '../../actions/posts'
+
 jest.mock('react-redux')
+jest.mock('../../apis/comments')
+jest.mock('../../actions/posts')
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
+useSelector.mockReturnValue({
+  id: 5,
+  username: 'Aaron123',
+})
 
 describe('<Comment />', () => {
   it('renders the username of the commenter and the comment', () => {
     render(
       <Comment
         key={1}
+        commentId={1}
         commenter={'Aaron123'}
-        commentDate={'Sat 24 September 2022'}
+        userId={5}
+        commentDate={'Sat Sep 24 2022'}
         comment={'I found your basketball'}
         postId={2}
       />
@@ -19,18 +37,30 @@ describe('<Comment />', () => {
     expect(comment).toHaveLength(1)
     expect(comment[0].textContent).toContain('Aaron123')
     expect(comment[0].textContent).toContain('basketball')
-    expect(comment[0].textContent).toContain('24 September')
+    expect(comment[0].textContent).toContain('Sat Sep 24')
   })
-})
 
-describe('<Comments />', () => {
-  test('renders the mocked data on the server', () => {
-    useSelector.mockReturnValue([{ id: 1 }, { comment: 'test' }])
-    useDispatch.mockReturnValue(() => {
-      render(<Comment />)
-      const commentsBody = screen.getAllByRole('listitem')
-      expect(commentsBody[1]).toHaveTextContent('test')
-      expect(commentsBody).toHaveLength(2)
-    })
+  test('clicking Delete comment button will call deleteComment function and dispatch fetchPosts', async () => {
+    deleteComment.mockReturnValue('Comment deleted')
+    const fakeDispatch = jest.fn()
+    useDispatch.mockReturnValue(fakeDispatch)
+    fetchPosts.mockReturnValue('Posts to fetch')
+
+    render(
+      <Comment
+        key={1}
+        commentId={1}
+        commenter={'Aaron123'}
+        userId={5}
+        commentDate={'Sat Sep 24 2022'}
+        comment={'I found your basketball'}
+        postId={2}
+      />
+    )
+    const deleteCommentButton = screen.getByRole('button')
+
+    await userEvent.click(deleteCommentButton)
+    expect(deleteComment).toHaveBeenCalled()
+    expect(fakeDispatch).toHaveBeenCalledWith('Posts to fetch')
   })
 })
