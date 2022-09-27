7bae9a34f3e4923231d6192c3a10bd3dccd467ae
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-09-28 09:42:19 +1300

    amend migrations and seeds for comments

--- server/db/seeds/3-comments.js
+++ server/db/seeds/3-comments.js
@@ -4,14 +4,14 @@ exports.seed = function (knex) {
       id: 1,
       commenter_id: '1',
       post_id: 1,
-      date_commented: new Date().toDateString(),
+      date_commented: Date.now(),
       comment: 'I think this could be mine - I lost a Gorman one',
     },
     {
       id: 2,
       commenter_id: '1',
       post_id: 1,
-      date_commented: new Date().toDateString(),
+      date_commented: Date.now(),
       comment: 'Never mind, I found mine!',
     },
   ])
