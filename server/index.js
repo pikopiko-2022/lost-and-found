5ea61728a2391097c61d985a9f5947a248569e41
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-09-27 19:38:38 +1300

    node_env

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ const server = require('./server')
 
 const PORT = process.env.PORT || 2000
 
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  const envConfig = require('dotenv').config()
+  if (envConfig.error) throw envConfig.error
+}
+
 server.listen(PORT, () => {
   console.log('Server listening on port', PORT)
 })
