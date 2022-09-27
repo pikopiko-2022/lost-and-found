e135bdd48ee9c29a9a1e3d3299679138ce66274b
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-09-27 15:59:56 +1300

    try commenting out reference

--- server/db/migrations/20220921030255_comments.js
+++ server/db/migrations/20220921030255_comments.js
@@ -2,7 +2,8 @@ exports.up = function (knex) {
   return knex.schema.createTable('comments', (table) => {
     table.increments('id')
     table.integer('post_id').references('posts.id')
-    table.string('commenter_id').references('users.id')
+    table.string('commenter_id')
+    // .references('users.id')
     table.date('date_commented')
     table.string('comment')
   })
