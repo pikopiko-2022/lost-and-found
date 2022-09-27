0e70c2a4c354a180cdf46fa2328016eb7729fc4a
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-09-27 15:40:11 +1300

    amend migration reference

--- server/db/migrations/20220921030255_comments.js
+++ server/db/migrations/20220921030255_comments.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('comments', (table) => {
     table.increments('id')
     table.integer('post_id').references('posts.id')
-    table.string('commenter_id').references('users.auth0_id')
+    table.string('commenter_id').references('users.id')
     table.date('date_commented')
     table.string('comment')
   })
